e3ba0836cf4e295c798de067845bcc988689ea50
Vitor Ochoa <vitorochoa12@gmail.com>
2021-03-30 16:19:16 -0300

    Tratando erros com excecao personalizada

--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -8,14 +8,22 @@ namespace Chass_Console
     {
         static void Main(string[] args)
         {
-            Board board = new Board(8, 8);
-            board.putPiece(new Tower(board, Color.White), new Position(0, 0));
-            board.putPiece(new Tower(board, Color.Black), new Position(1, 3));
-            board.putPiece(new King(board, Color.Black), new Position(2, 4));
+            try
+            {
+                Board board = new Board(8, 8);
+                board.putPiece(new Tower(board, Color.White), new Position(0, 0));
+                board.putPiece(new Tower(board, Color.Black), new Position(1, 3));
+                board.putPiece(new King(board, Color.Black), new Position(2, 4));
+                board.putPiece(new Tower(board, Color.White), new Position(0, 2));
 
-            Screen.printBoard(board);
-
+                Screen.printBoard(board);
+            }
+            catch (BoardException e)
+            {
+                Console.WriteLine(e.Message);
+            } 
             Console.ReadLine();
+            
         }
     }
 }
--- Xadrez_Console/board/Board.cs
+++ Xadrez_Console/board/Board.cs
@@ -18,10 +18,43 @@
             return Pieces[line, colum];
         }
 
+        public Piece piece(Position pos)
+        {
+            return Pieces[pos.colum, pos.line];
+        }
+
+        public bool existPiece(Position pos)
+        {
+            validatePosition(pos);
+            return piece(pos) != null;
+
+        }
+
         public void putPiece(Piece p, Position pos)
         {
+            if (existPiece(pos))
+            {
+                throw new BoardException("Já existe uma Peça nessa posição!");
+            }           
             Pieces[pos.line, pos.colum] = p;
             p.position = pos;
+        }
+
+        public bool positionValid(Position pos)
+        {
+            if(pos.line < 0|| pos.line >= Lines || pos.colum < 0 || pos.colum >= Colums)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void validatePosition(Position pos)
+        {
+            if (!positionValid(pos))
+            {
+                throw new BoardException("Posição Inválida!");
+            }
         }
     }
 }
